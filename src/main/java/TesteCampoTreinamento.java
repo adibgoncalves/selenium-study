90624e86de6ab0c49953845a91a67fd770000064
Adib Gonçalves Fernandes de Souza <adib.fernandes@gmail.com>
2022-03-10 08:50:20 -0300

    Subindo código até a aula 24

---
+++ src/main/java/TesteCampoTreinamento.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteCampoTreinamento {
+
+	@Test
+	public void Teste() {
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\dev\\selenium\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200,765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		//driver.quit();
+	}
+}
